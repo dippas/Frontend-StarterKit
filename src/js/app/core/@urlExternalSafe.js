e302b8fc3085e41f4d93f9609cec221171deb303
Serpa <serpa14@gmail.com>
2021-01-21 14:59:49 +0000

    UPD core JS

--- src/js/app/core/@urlExternalSafe.js
+++ src/js/app/core/@urlExternalSafe.js
@@ -4,20 +4,8 @@ const url = {
 		linkToNewTab: document.querySelectorAll('a[target="_blank"]')
 	},
 
-	fxNewTab(url) {
-		const newTab = window.open()
-		newTab.opener = null
-		newTab.location = url
-	},
-
 	events() {
-		this.el.linkToNewTab.forEach(el => {
-			el.setAttribute('rel', 'noopener noreferrer')
-			el.addEventListener('click', e => {
-				this.fxNewTab(e.currentTarget)
-				return false
-			})
-		})
+		this.el.linkToNewTab.forEach(el => el.setAttribute('rel', 'noopener noreferrer'))
 	},
 
 	init() {
