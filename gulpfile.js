43b4883b2f8a7a7a3c02195f61e08093c1ed43b0
Fábio Serpa <serpa14@gmail.com>
2021-06-10 02:31:33 +0100

    UPD gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,8 @@ const { series } = require('gulp'),
 	scriptsFile = require('./gulp/scripts'),
 	serverFile = require('./gulp/server'),
 	stylesFile = require('./gulp/styles'),
-	videosFile = require('./gulp/videos'),
-	dev = series(cleanFile.clean, stylesFile.styles, scriptsFile.scripts, htmlFile.html, extrasFile.extras, imagesFile.images, videosFile.videos, stylesFile.sassLinter)
+	dev = series(cleanFile.clean, stylesFile.styles, scriptsFile.scripts, htmlFile.html, extrasFile.extras, imagesFile.images, stylesFile.sassLinter)
 
-//--------- Create tasks	
+//--------- Create tasks
 exports.build = dev
 exports.default = serverFile.server
